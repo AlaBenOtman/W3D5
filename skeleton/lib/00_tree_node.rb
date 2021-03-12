2e8bc850e30e0ef702231540f1b2402348f2c97f
voldemort <alabenothman@gmail.com>
2021-03-12 11:32:39 -0800

    Created parent= & initialize methods

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -6,7 +6,16 @@ class PolyTreeNode
         @value = value 
     end
 
-    # def parents=(parents)
-    #     parents.children << self
-    # end
+    def parent=(parent)
+        if @parent.nil? 
+            @parent = parent
+        else 
+            @parent.children.delete(self)
+        end
+        unless parent.children.include?(self)
+            parent.children << self 
+        end
+    end
+
+
 end
