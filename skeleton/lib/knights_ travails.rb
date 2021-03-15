5d4315ca6c3834f228fe47cd5fedc853fc80f840
chrisweeting <chrisweeting@gmail.com>
2021-03-15 16:01:58 -0400

    Added valid_moves

--- skeleton/lib/knights_ travails.rb
+++ skeleton/lib/knights_ travails.rb
@@ -1,15 +1,43 @@
 require_relative "./00_tree_node.rb"
 
 class KnightPathFinder 
-    attr_reader :start_pos, :root_node
+    attr_reader :start_pos, :root_node, :considered_positions
+    POSS_MOVES = {
+        pos_1: Proc.new {|r, c| [(r - 1), (c - 2)]},
+        pos_2: Proc.new {|r, c| [(r - 2), (c - 1)]},
+        pos_3: Proc.new {|r, c| [(r + 1), (c - 2)]},
+        pos_4: Proc.new {|r, c| [(r + 2), (c - 1)]},
+        pos_5: Proc.new {|r, c| [(r + 2), (c + 1)]},
+        pos_6: Proc.new {|r, c| [(r + 1), (c + 2)]},
+        pos_7: Proc.new {|r, c| [(r - 1), (c + 2)]},
+        pos_8: Proc.new {|r, c| [(r - 1), (c + 1)]}
+    }
 
     def self.valid_moves(pos)
-        @considered_positions = [@start_pos]
+        row, col = pos
+
+        POSS_MOVES.each do |k, v|
+            if KnightPathFinder.valid?(v.call(row, col))
+                @considered_positions << v.call(row, col)
+            end
+        end
+        # make hash/arr procs representing possible moves
+        # iterate through poss_moves if a move is valid it is selected
+        # return an array of valid possible moves
+
     end
 
+    def self.valid?(pos)
+        # return a bool if valid
+        pos.all? do |ele|
+            ele >= 0 && ele < 8
+        end
+    end
+    
     def initialize(start_pos)
         @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = []
         
     end
 
@@ -22,7 +50,16 @@ class KnightPathFinder
     end
 
 
+    # 00  01  02  03  04  05  06  07
+    # 10  11  12  13  14  15  16  17
+    # 20  21  22  23  24  25  26  27
+    # 30  31  32  33  34  35  36  37
+    # 40  41  42  43  44  45  46  47
+    # 50  51  52  53  54  55  56  57
+    # 60  61  62  63  64  65  66  67
+    # 70  71  72  73  74  75  76  77
 
+    
 
 
 
