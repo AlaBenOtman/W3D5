6db92a99af72ca67d0529c62bd02cfbce4dfe3f3
chrisweeting <chrisweeting@gmail.com>
2021-03-15 13:49:22 -0400

    Finished BFS

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -31,7 +31,7 @@ class PolyTreeNode
     end
 
     def add_child(child)
-        child.parent=(self)
+        child.parent = self
     end
 
     def remove_child(child)
@@ -50,23 +50,42 @@ class PolyTreeNode
     end
 
     def bfs(target_value)
-        debugger
+        # debugger
         array = [self]
         until array.empty?
             first = array.shift
             if first.value == target_value
                  return first
             else
-                array << first.children
+                array += first.children
             end
         end
         nil
     end
 
     def inspect 
-        "<#{self.value} //#{self.children} //#{self.parent.value} // >"
+        "<Value:#{self.value}><Childern:#{self.children}><Parent:#{self.parent.value}>"
+        # "<Childern:#{self.children}>" 
+        # "<Parent:#{self.parent.value}>"
     end
 
-
 end
+
+        #         n1
+        #     /       \
+        #     n2      n3
+        # /     \       \
+        # n4    n5       n6
+
+n1 = PolyTreeNode.new("1")
+n2 = PolyTreeNode.new("2")
+n3 = PolyTreeNode.new("3")
+n4 = PolyTreeNode.new("4")
+n5 = PolyTreeNode.new("5")
+n6 = PolyTreeNode.new("6")
+n6.parent = n3
+n5.parent = n2
+n4.parent = n2
+n2.parent = n1
+n3.parent = n1
 
